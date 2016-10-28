cf00845cddde40a7e2585ed7bbe8f38c34391899
Da Xing <d.x@fowtech.com>
2016-10-29 01:51:09 +0800

    fix some usage.

--- app/src/main/java/com/xdandroid/hellodaemon/misc/RevokeActivity.java
+++ app/src/main/java/com/xdandroid/hellodaemon/misc/RevokeActivity.java
@@ -1,6 +1,5 @@
 package com.xdandroid.hellodaemon.misc;
 
-import android.annotation.*;
 import android.app.*;
 import android.content.pm.*;
 import android.os.*;
@@ -15,53 +14,54 @@ import java.util.*;
  */
 public class RevokeActivity extends Activity {
 
-    @TargetApi(Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         new Thread(() -> {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return;
             PackageManager pm = getPackageManager();
             List<PackageInfo> installedPackages = pm.getInstalledPackages(PackageManager.GET_PERMISSIONS);
-            AppOpsManager appOps = getSystemService(AppOpsManager.class);
+            Class<AppOpsManager> aomClazz = AppOpsManager.class;
+            AppOpsManager aom = getSystemService(aomClazz);
             try {
                 Method setUidModeMethod;
-                boolean marshmallow = false;
+                boolean nougat = true;
                 Method permissionToOpCodeMethod = null;
                 try {
-                    setUidModeMethod = AppOpsManager.class.getMethod("setUidMode", String.class, int.class, int.class);
-                } catch (NoSuchMethodException e) {
-                    marshmallow = true;
-                    setUidModeMethod = AppOpsManager.class.getMethod("setUidMode", int.class, int.class, int.class);
-                    permissionToOpCodeMethod = AppOpsManager.class.getMethod("permissionToOpCode", String.class);
+                    setUidModeMethod = aomClazz.getMethod("setUidMode", String.class, int.class, int.class);
+                } catch (Exception e) {
+                    nougat = false;
+                    setUidModeMethod = aomClazz.getMethod("setUidMode", int.class, int.class, int.class);
+                    permissionToOpCodeMethod = aomClazz.getMethod("permissionToOpCode", String.class);
                 }
-                Method setModeMethod = AppOpsManager.class.getMethod("setMode", int.class, int.class, String.class, int.class);
-                for (PackageInfo packageInfo : installedPackages) {
-                    if ((packageInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0 ||
-                            (packageInfo.applicationInfo.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) continue;
-                    if (packageInfo.applicationInfo.targetSdkVersion >= Build.VERSION_CODES.M) continue;
-                    int uid = packageInfo.applicationInfo.uid;
-                    if (packageInfo.requestedPermissions == null || packageInfo.requestedPermissions.length <= 0) continue;
-                    for (String requestedPermission : packageInfo.requestedPermissions) {
+                for (PackageInfo pi : installedPackages) {
+                    if ((pi.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0 ||
+                            (pi.applicationInfo.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) continue;
+                    if (pi.applicationInfo.targetSdkVersion >= Build.VERSION_CODES.M) continue;
+                    int uid = pi.applicationInfo.uid;
+                    if (pi.requestedPermissions == null || pi.requestedPermissions.length <= 0) continue;
+                    for (String reqPermission : pi.requestedPermissions) {
                         PermissionInfo permissionInfo = null;
                         try {
-                            permissionInfo = pm.getPermissionInfo(requestedPermission, 0);
-                        } catch (PackageManager.NameNotFoundException ignored) {}
+                            permissionInfo = pm.getPermissionInfo(reqPermission, 0);
+                        } catch (Exception ignored) {}
                         if (permissionInfo == null || permissionInfo.protectionLevel != PermissionInfo.PROTECTION_DANGEROUS) continue;
                         if (!permissionInfo.name.startsWith("android")) continue;
-                        if (!marshmallow) {
-                            String appOp = AppOpsManager.permissionToOp(permissionInfo.name);
-                            if (!TextUtils.isEmpty(appOp)) setUidModeMethod.invoke(appOps, appOp, uid, AppOpsManager.MODE_IGNORED);
+                        if (nougat) {
+                            String op = AppOpsManager.permissionToOp(permissionInfo.name);
+                            if (!TextUtils.isEmpty(op)) setUidModeMethod.invoke(aom, op, uid, AppOpsManager.MODE_IGNORED);
                         } else {
                             try {
-                                int code = (int) permissionToOpCodeMethod.invoke(appOps, permissionInfo.name);
-                                setUidModeMethod.invoke(appOps, code, uid, AppOpsManager.MODE_IGNORED);
+                                int code = (int) permissionToOpCodeMethod.invoke(aom, permissionInfo.name);
+                                setUidModeMethod.invoke(aom, code, uid, AppOpsManager.MODE_IGNORED);
                             } catch (Exception e) {
                                 e.printStackTrace();
                             }
                         }
                     }
-                    setModeMethod.invoke(appOps, 24, uid, packageInfo.applicationInfo.packageName, AppOpsManager.MODE_IGNORED);
-                    setModeMethod.invoke(appOps, 23, uid, packageInfo.applicationInfo.packageName, AppOpsManager.MODE_IGNORED);
+                    Method setModeMethod = aomClazz.getMethod("setMode", int.class, int.class, String.class, int.class);
+                    setModeMethod.invoke(aom, 24, uid, pi.applicationInfo.packageName, AppOpsManager.MODE_IGNORED);
+                    setModeMethod.invoke(aom, 23, uid, pi.applicationInfo.packageName, AppOpsManager.MODE_IGNORED);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
