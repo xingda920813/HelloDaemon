c8003ce69a17b803318fc8db24861fc0f9472f2f
Da Xing <d.x@fowtech.com>
2016-11-16 23:12:02 +0800

    WakeLock 使用修正.

--- app/src/main/java/com/xdandroid/hellodaemon/receiver/WakeLockReceiver.java
+++ app/src/main/java/com/xdandroid/hellodaemon/receiver/WakeLockReceiver.java
@@ -19,20 +19,21 @@ public class WakeLockReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent == null || intent.getAction() == null || WatchDogService.sWakeLock == null)
+        //若订阅已经取消，说明任务不在运行，已没有必要获取 WakeLock。为节省电量，释放之前获得的 WakeLock。
+        if ((WorkService.sSubscription == null || WorkService.sSubscription.isUnsubscribed()) &&
+                WatchDogService.sWakeLock != null && WatchDogService.sWakeLock.isHeld()) {
+            try {WatchDogService.sWakeLock.release();} catch (Exception ignored) {}
             return;
+        }
+        if (intent == null || intent.getAction() == null || WatchDogService.sWakeLock == null) return;
         switch (intent.getAction()) {
             case Intent.ACTION_SCREEN_OFF:
                 //关闭屏幕时获得 WakeLock
-                try {
-                    if (!WatchDogService.sWakeLock.isHeld()) WatchDogService.sWakeLock.acquire();
-                } catch (Exception ignored) {}
+                try {if (!WatchDogService.sWakeLock.isHeld()) WatchDogService.sWakeLock.acquire();} catch (Exception ignored) {}
                 break;
             case Intent.ACTION_SCREEN_ON:
                 //点亮屏幕后释放 WakeLock
-                try {
-                    if (WatchDogService.sWakeLock.isHeld()) WatchDogService.sWakeLock.release();
-                } catch (Exception ignored) {}
+                try {if (WatchDogService.sWakeLock.isHeld()) WatchDogService.sWakeLock.release();} catch (Exception ignored) {}
                 break;
         }
     }
