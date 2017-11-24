06e6b30dd67aa8b0a05f18da1968587c4d4296e2
Da Xing <570396198@qq.com>
2017-11-24 17:19:15 +0800

    Android 8.0 onBindingDied()

--- hellodaemon/src/main/java/com/xdandroid/hellodaemon/DaemonEnv.java
+++ hellodaemon/src/main/java/com/xdandroid/hellodaemon/DaemonEnv.java
@@ -39,16 +39,21 @@ public final class DaemonEnv {
         ServiceConnection bound = BIND_STATE_MAP.get(serviceClass);
         if (bound == null) sApp.bindService(i, new ServiceConnection() {
             @Override
-            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            public void onServiceConnected(ComponentName name, IBinder service) {
                 BIND_STATE_MAP.put(serviceClass, this);
             }
 
             @Override
-            public void onServiceDisconnected(ComponentName componentName) {
+            public void onServiceDisconnected(ComponentName name) {
                 BIND_STATE_MAP.remove(serviceClass);
                 startServiceSafely(i);
                 if (!sInitialized) return;
                 sApp.bindService(i, this, Context.BIND_AUTO_CREATE);
+            }
+            
+            @Override
+            public void onBindingDied(ComponentName name) {
+                onServiceDisconnected(name);
             }
         }, Context.BIND_AUTO_CREATE);
     }
